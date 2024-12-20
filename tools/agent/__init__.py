b34cc68ba6da3ad652231f2605f5d389f6b704b4
bang666asik <kevinhumala012@gmail.com>
2024-12-20 15:00:00 +0000

    Implement multi-agent collaboration system

--- tools/agent/__init__.py
+++ tools/agent/__init__.py
@@ -1,6 +1,5 @@
 from .list_available_agents import list_available_agents
 from .assign_agent_to_task import assign_agent_to_task
 
-__all__ = [
     'list_available_agents',
     'assign_agent_to_task'
