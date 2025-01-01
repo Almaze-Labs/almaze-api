66f3d718fcfc62e40cb58f691327655ce11b1ea6
bang666asik <kevinhumala012@gmail.com>
2025-01-01 17:00:00 +0000

    Create dynamic UI components

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,10 +1,8 @@
-from .compass import compass
 from .tool_smith import tool_smith
 from .architect import architect
 from .scout import scout
 from .techsage import techsage
 __all__ = [
-    'compass',
     'tool_smith',
     'architect',
     'scout',
--- tools/file/__init__.py
+++ tools/file/__init__.py
@@ -1,8 +1,5 @@
 from .write_to_file import write_to_file
-from .read_file import read_file
 from .delete_file import delete_file
-from .overwrite_file import overwrite_file
 
-__all__ = [
     'write_to_file',
     'delete_file',
